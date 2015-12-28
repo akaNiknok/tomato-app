03d2d290fedace0c14b67a18c1628d99d0e009fe
akaNiknok <austingimperial@gmail.com>
2015-12-28 12:12:54 +0800

    Users can now login

--- tomato.py
+++ tomato.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, session, redirect
+from flask import Flask, render_template, request, url_for, session, redirect, make_response
 import sqlite3
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         return redirect("/" + request.form["country"])
-    return render_template("index.html")
+    return render_template("index.html", user=request.cookies.get("username"))
 
 @app.route("/<country>")
 def country(country):
@@ -16,7 +16,19 @@ def country(country):
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        pass
+        form = request.form
+
+        db = sqlite3.connect("database.db")
+        cursor = db.cursor()
+        cursor.execute("SELECT password FROM users WHERE name=?", (form["user"],))
+        if cursor.fetchone()[0] != form["pwd"]:
+            db.close()
+            return render_template("login.html", error=True)
+
+        resp = make_response(redirect("/"))
+        resp.set_cookie("username", form["user"])
+        db.close()
+        return resp
     return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
